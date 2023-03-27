242a7a1f5f90a194a7841ea76ca4ca7d945a2298
amirhrb <amirrabi70@gmail.com>
2023-03-28 00:39:46 +0430

    debug some misses

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,24 @@
 //react
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 //next
-import Head from "next/head";
 import { useRouter } from "next/router";
 
 //style
 import styles from "../helper/styles/index.module.css";
 
+import ReactMarkdown from "react-markdown";
+
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+
 export default function Home() {
   const router = useRouter();
-  const [messages, setMessages] = useState("");
+
+  const inputRef = useRef(null);
+  const containerRef = useRef(null);
+
+  const [messages, setMessages] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [result, setResult] = useState(["", ""]);
 
   useEffect(() => {
     const authorized = async () => {
@@ -32,44 +38,110 @@ export default function Home() {
   }, []);
 
   async function onSubmit(event) {
-    setLoading(true);
     event.preventDefault();
+    if (!inputRef.current.value) {
+      alert("ask a question!");
+      return;
+    }
+    setLoading(true);
     try {
+      // console.log("here");
       const response = await fetch("/api/generate", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ messages: messages }),
+        body: JSON.stringify({
+          messages: [
+            ...messages,
+            { role: "user", content: inputRef.current.value },
+          ],
+        }),
       });
-      setPrompt("");
       const data = await response.json();
-      console.log(data);
       if (response.status !== 200) {
         throw (
           data.error ||
           new Error(`Request failed with status ${response.status}`)
         );
       }
-      setResult(() => data.result);
+      setMessages((prev) => [...prev, data.question, data.result]);
+      inputRef.current.value = "";
       setLoading(false);
     } catch (error) {
       setLoading(false);
       alert(error.message);
     }
   }
+  useEffect(() => {
+    window.scrollTo({
+      top: containerRef.current.scrollHeight,
+      behavior: "smooth",
+    });
+    console.log(messages);
+  }, [messages]);
 
   return (
     <>
       <main className={styles.main}>
-        <img src="/svg/chatGPT.svg" className={styles.icon} />
-        <h3>Answer my question</h3>
-        <form onSubmit={onSubmit}>
-          <input
+        <div
+          className={messages.length ? styles.iconCont : ""}
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <img src="/svg/chatGPT.svg" className={styles.icon} />
+          <h3>Answer my question</h3>
+        </div>
+        <div
+          className={messages.length ? styles.resultCont : ""}
+          ref={containerRef}
+        >
+          {messages.length
+            ? messages.map((message, index) => (
+                <div
+                  key={index}
+                  className={styles.resultItem}
+                  dir={/[\u0591-\u07FF]/.test(message.content) ? "rtl" : "ltr"}
+                >
+                  <h4>{message.role === "user" ? "YOU:" : "AI:"}</h4>
+                  <div className={styles.result}>
+                    <ReactMarkdown
+                      children={message.content}
+                      components={{
+                        code({ node, inline, className, children, ...props }) {
+                          const match = /language-(\w+)/.exec(className || "");
+                          return !inline && match ? (
+                            <SyntaxHighlighter
+                              children={String(children).replace(/\n$/, "")}
+                              //   showLineNumbers
+                              PreTag="section"
+                              language={match[1]}
+                              {...props}
+                            />
+                          ) : (
+                            <code className={className} {...props}>
+                              `{children}`
+                            </code>
+                          );
+                        },
+                      }}
+                    />
+                  </div>
+                </div>
+              ))
+            : ""}
+        </div>
+        <form
+          onSubmit={onSubmit}
+          className={messages.length ? styles.fixedForm : ""}
+        >
+          <textarea
             type="text"
             placeholder="Enter your qustion"
-            value={prompt}
-            onChange={(e) => setPrompt(e.target.value)}
+            ref={inputRef}
           />
           <input
             type="submit"
@@ -77,21 +149,6 @@ export default function Home() {
             disabled={loading}
           />
         </form>
-        <div
-          className={styles.resultCont}
-          dir={/[\u0591-\u07FF]/.test(result[1]) ? "rtl" : "ltr"}
-        >
-          {result[0] ? (
-            <>
-              <h4>You:</h4>
-              <code className={styles.question}>{result[0]}</code>
-              <h4>AI:</h4>
-              <code className={styles.result}>{result[1]}</code>
-            </>
-          ) : (
-            ""
-          )}
-        </div>
       </main>
     </>
   );
